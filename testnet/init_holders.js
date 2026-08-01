ca6408e653785f2035e1d758bf9a15ee2d6fb96b
Top <wangc_web3@163.com>
2026-08-01 09:16:26 +0800

    fix: set testnet initial supply to one billion DMT

--- testnet/init_holders.js
+++ testnet/init_holders.js
@@ -1,7 +1,7 @@
 const init_holders = [
   {
     address: '0x9C9B2D5982eD8b12Eb127f2358A734eB60271Dc0',
-    balance: BigInt('1000000000000000000000000000000').toString(16), // 1,000,000,000 DMT
+    balance: BigInt('1000000000000000000000000000').toString(16), // 1,000,000,000 DMT
   },
 ];
 
